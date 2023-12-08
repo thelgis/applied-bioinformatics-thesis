54f2e40c2ad8dc72a293eddc01d942c8e92b1e91
Thelgis Kotsos <thelgis@pm.me>
2023-12-08 22:05:27 +0200

    Determine common genes per group of datasets

---
+++ adex/helpers.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+from typing import List, Set
+
+from adex.type_aliases import Gene
+from adex.models import Condition
+from polars import DataFrame
+import polars as pl
+
+
+def load_data_per_condition(condition: Condition, path: str) -> List[DataFrame]:
+    """
+    Loads all the datasets of a certain condition in a list of dataframes
+    """
+
+    results = [
+        pl.read_parquet(file)
+        for file in Path(f"{path}/{condition.name}").glob('*.parquet')
+    ]
+
+    if len(results) == 0:
+        raise ValueError(f"Possibly wrong path '{path}' provided for files")
+
+    return results
+
+
+def gene_intersection(dataframes: List[DataFrame]) -> Set[Gene]:
+    """
+    Returns all the common genes found in a list of dataframes
+    """
+    common_genes = set()
+
+    for df in dataframes:
+        if len(common_genes) == 0:
+            common_genes.update(
+                df.select("gene").to_series().to_list()
+            )
+        else:
+            common_genes.intersection_update(
+                set(df.select("gene").to_series().to_list())
+            )
+
+    return common_genes
--- adex/models.py
+++ adex/models.py
@@ -2,10 +2,8 @@ from enum import Enum
 
 
 class Condition(Enum):
-    Healthy = 1
+    RA = 1
     T1D = 2
     SSc = 3
     SLE = 4
     SjS = 5
-    RA = 6
-
